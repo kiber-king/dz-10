72c52e12ae9157651482dda36a06da717cd20048
Gleb <gleb.kvasov26@mail.ru>
2022-05-06 18:18:04 +0300

    update

---
+++ dz 10/Human.cpp
@@ -0,0 +1,7 @@
+#include "Human.h"
+int main() {
+	Student student("Ivanov", "Ivan", "Sergeevich", 20);
+	student.Print();
+	Boss boss("Ivanov", "Mikhail", "Sergeevich", 40);
+	boss.Print();
+}
--- dz 10/Human.h
+++ dz 10/Human.h
@@ -1,1 +1,65 @@
 #pragma once
+#ifndef Human_H
+#define Human_H
+#include <iostream>
+class Human {
+protected:
+	std::string surname;
+	std::string name;
+	std::string midname;
+	int age;
+public:
+	Human(std::string surname=" ", std::string name=" ", std::string midname=" ", int age =18) {
+		this->surname = surname;
+		this->name = name;
+		this->midname = midname;
+		this->age = age;
+
+	}
+	~Human() {
+		std::cout << "Dest hum" << "\n";
+	}
+	virtual void Print() = 0;
+};
+
+class Student : public Human
+{
+	bool on_lesson;
+public:
+	Student(std::string surname = " ", std::string name = " ", std::string midname = " ", int age = 18)
+	{
+		this->surname = surname;
+		this->name = name;
+		this->midname = midname;
+		this->age = age;
+	}
+	~Student() {
+		std::cout << "Dest stud" << "\n";
+	}
+	void Print() override {
+		std::cout << surname << " " << name << " " << midname << " " << age << " " << "\n";
+	}
+
+};
+
+class Boss :public Human {
+protected:
+	int number_of_works;
+public:
+	Boss(std::string surname = " ", std::string name = " ", std::string midname = " ", int age = 18)
+	{
+		this->surname = surname;
+		this->name = name;
+		this->midname = midname;
+		this->age = age;
+	}
+	~Boss() {
+		std::cout << "Dest boss" << "\n";
+	}
+	void Print() override {
+		std::cout << surname << " " << name << " " << midname << " " << age << " " << "\n";
+	}
+
+};
+
+#endif
